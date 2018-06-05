1aa568afbb7a9e356fb075930823108c58067eef
Lenovo <Lenovo@N409-3>
2018-06-05 08:10:04 +0800

    SSM框架整合入门

---
+++ src/com/itheima/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.itheima.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itheima.po.Customer;
+import com.itheima.service.CustomerService;
+
+
+@Controller
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	/**
+	 * 根据id查询客户详情
+	 */
+	@RequestMapping(value="/findCustomerById")
+	public String findCustomerById(Integer id, Model model) {
+		Customer customer = this.customerService.findCustomerById(id);
+		model.addAttribute("customer", customer);
+		//返回客户信息展示页面
+		return "customer";
+	}
+}
---
+++ src/com/itheima/po/Customer.java
@@ -0,0 +1,38 @@
+package com.itheima.po;
+
+/**
+ * 客户持久化类
+ */
+public class Customer {
+	private Integer id;//用户ID
+	private String username;//用户姓名
+	private String jobs;//职业
+	private String phone;//电话
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getJobs() {
+		return jobs;
+	}
+	public void setJobs(String jobs) {
+		this.jobs = jobs;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+}
